ad7386668ebef818c4c651a28e3c6ec2839fd768
Rafa Canosa <rafacv23@gmail.com>
2023-07-05 16:01:33 +0200

    Exercise 1.7 done, added display for all, average and positive responses

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -8,23 +8,46 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
 
   const updateGood = () => {
     const newValor = good + 1
+    const valorTotal = all + 1
+    const valorAverage = average + 1
+    setAverage(valorAverage)
+    setAll(valorTotal);
     setGood(newValor);
     console.log(newValor);
+    console.log(valorTotal)
   }
 
   const updateNeutral = () => {
     const newValor = neutral + 1
+    const valorTotal = all + 1
+    const valorAverage = average + 0
+    setAverage(valorAverage)
+    setAll(valorTotal);
     setNeutral(newValor);
     console.log(newValor);
+    console.log(valorTotal)
   }
 
   const updateBad = () => {
     const newValor = bad + 1
+    const valorTotal = all + 1
+    const valorAverage = average - 1
+    setAverage(valorAverage)
+    setAll(valorTotal);
     setBad(newValor);
     console.log(newValor);
+    console.log(valorTotal)
+  }
+
+  const positiveComments = (good, all) => {
+    const positiveCommentsTotal = (good / all) * 100
+    const positiveCommentsTotalTruncated = Number(positiveCommentsTotal.toFixed(2))
+    return positiveCommentsTotalTruncated +"%"
   }
 
   return (
@@ -36,7 +59,8 @@ function App() {
       <ReactBtn handleClick={updateBad} name={"Bad"}></ReactBtn>
       </div>
       <Title name={"Stadistics"}></Title>
-      <Display contadorGood={good} contadorNeutral={neutral} contadorBad={bad}></Display>
+      <Display contadorGood={good} contadorNeutral={neutral} contadorBad={bad} 
+      contadorAll={all} contadorAverage={average} contadorPositive={positiveComments(good, all)}></Display>
     </div>
   );
 }
--- part01/unicafe/src/containers/display.jsx
+++ part01/unicafe/src/containers/display.jsx
@@ -7,6 +7,9 @@ const Display = (props) => {
             <Text text={"Good"} contador={props.contadorGood} ></Text>
             <Text text={"Neutral"} contador={props.contadorNeutral}></Text>
             <Text text={"Bad"} contador={props.contadorBad}></Text>
+            <Text text={"All"} contador={props.contadorAll}></Text>
+            <Text text={"Average"} contador={props.contadorAverage}></Text>
+            <Text text={"Positive"} contador={props.contadorPositive}></Text>
         </div>
     );
 }
