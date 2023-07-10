4f4ecd317312397e538fb2eda928cbcc8ca67490
Rafa Canosa <rafacv23@gmail.com>
2023-07-10 16:56:11 +0200

    Exercise 2.6 done

--- part02/phone-book/src/App.js
+++ part02/phone-book/src/App.js
@@ -1,24 +1,40 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  /*const [ persons, setPersons ] = useState([
+  const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas' }
   ]) 
-  const [ newName, setNewName ] = useState('')*/
+  const [ newPerson, setNewPerson ] = useState('')
+
+  const createPerson = (event) => {
+    console.log(event.target.value)
+    setNewPerson(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const person = {
+      name: newPerson,
+      id: persons.length + 1,
+    }
+      setPersons(persons.concat(person))
+    }
 
   return (
     <div className='body'>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson} >
         <div>
-          name: <input />
+          name: <input value={newPerson} onChange={createPerson} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <div key={person.id}> {person.name} </div>
+      ))}
     </div>
   )
 }
