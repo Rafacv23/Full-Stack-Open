34dee3e5755643acd1f834b5e01ed121707703fe
Rafa Canosa <rafacv23@gmail.com>
2023-07-19 12:40:52 +0200

    exercise 2.9 done, added filters to the search

--- part02/phone-book/src/App.js
+++ part02/phone-book/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Header from './components/header'
 import AddForm from './containers/add-form'
 import ContactDisplay from './containers/contact-display'
+import Input from './components/input'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
@@ -9,6 +10,8 @@ const App = () => {
   ]) 
   const [ newPerson, setNewPerson ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ searchPerson, setSearchPerson ] = useState('')
+  const [ filteredPersons, setFilteredPersons ] = useState('')
 
   const createPerson = (event) => {
     console.log(event.target.value)
@@ -33,7 +36,7 @@ const App = () => {
       alert("Error: Number length must be 9 characters.");
     } else {
       const person = {
-        name: newPerson,
+        name: newPerson.charAt(0).toUpperCase() + newPerson.slice(1),
         id: persons.length + 1,
         number: newNumber
       };
@@ -41,15 +44,29 @@ const App = () => {
     }
   }
 
+  const createSearchPerson = (event) => {
+    const searchTerm = event.target.value
+    const filteredSearchTerm = searchTerm.charAt(0).toUpperCase() + searchTerm.slice(1)
+    setSearchPerson(filteredSearchTerm)
+    filterPerson(filteredSearchTerm)
+  }
+
+  const filterPerson = (searchTerm) => {
+    const filtered = persons.filter((person) => 
+    person.name === searchTerm)
+    setFilteredPersons(filtered);
+  }
+
   return (
     <div className='body'>
       <Header name={"Phonebook"}></Header>
         <AddForm onSubmit={addPerson} value={newPerson} onChange={createPerson} valueNumber={newNumber} onChangeNumber={createNumber}></AddForm>
       <Header name={"Contacts"}></Header>
-      <ContactDisplay array={persons}>
-        {persons.map((person) => (
-          <div key={person.id}>{person.name}</div>
-        ))}
+      <Input value={searchPerson} onChange={createSearchPerson} placeholder={"Search"}></Input>
+      <ContactDisplay array={filteredPersons.length > 0 ? filteredPersons : persons}>
+        {filteredPersons.length > 0
+          ? filteredPersons.map((person) => <div key={person.id}>{person.name}</div>)
+          : persons.map((person) => <div key={person.id}>{person.name}</div>)}
       </ContactDisplay>
     </div>
   )
